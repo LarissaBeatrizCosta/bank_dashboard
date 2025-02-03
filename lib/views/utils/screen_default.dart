8a5414ccc779ce8cf590e68d91a2ce3a3007ea56
larissa beatriz costa <larissa@grupovex.com.br>
2025-02-03 10:32:14 -0300

    layout da app bar pronto

--- lib/views/utils/screen_default.dart
+++ lib/views/utils/screen_default.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:viacred_app/views/utils/colors.dart';
 
+import 'colors.dart';
+
+///Tela default da dash
 class ScreenDefault extends StatelessWidget {
+  ///Construtor
   const ScreenDefault({super.key, required this.child});
 
   ///Widget child
@@ -9,26 +12,38 @@ class ScreenDefault extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _style = TextStyle(
-      fontSize: 16,
+    var screenWidth = MediaQuery.of(context).size.width;
+    final style = TextStyle(
+      fontSize: (screenWidth > 600 ? 23 : 20),
       fontWeight: FontWeight.bold,
       color: ColorsHome().colorMap[11],
     );
-    final _text = Text('Dashboard', style: _style);
+    final text = Padding(
+      padding: (screenWidth > 600 ? EdgeInsets.all(20) : EdgeInsets.all(8)),
+      child: Text('Dashboard', style: style),
+    );
     return Scaffold(
       appBar: AppBar(
-        title: _text,
+        title: text,
+        actions: screenWidth > 600
+            ? [
+                _Row(),
+                Container(
+                  margin: const EdgeInsets.only(
+                      left: 50, right: 30, top: 5, bottom: 5),
+                  child: Image.asset(
+                    'assets/images/logo_white2.png',
+                    fit: BoxFit.contain,
+                    height: 500,
+                    width: 100,
+                  ),
+                )
+              ]
+            : null,
         automaticallyImplyLeading: false,
         backgroundColor: ColorsHome().colorMap[13],
       ),
-      body: Stack(
-        children: [
-          _ImageBackground(),
-          Container(
-            child: child,
-          ),
-        ],
-      ),
+      body: _Body(child: child),
     );
   }
 }
@@ -43,6 +58,50 @@ class _ImageBackground extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _ImageBackground(),
+        Container(
+          child: child,
+        ),
+      ],
+    );
+  }
+}
+
+class _Row extends StatelessWidget {
+  const _Row();
+
+  @override
+  Widget build(BuildContext context) {
+    var nomeProvisorio = 'Nome provisório';
+    var screenWidth = MediaQuery.of(context).size.width;
+    final style = TextStyle(
+      fontSize: 14,
+      color: ColorsHome().colorMap[11],
+    );
+
+    final text = Text(
+      'Bem vindo \n$nomeProvisorio',
+      textAlign: TextAlign.start,
+      style: style,
+    );
+
+    return Row(
+      children: [
+        text,
+      ],
     );
   }
 }
